7b352bbaa062f295ddc10157d759f4e891c06b05
Lixeer <1612655510@qq.com>
2025-11-09 01:05:42 +0800

    feat:更完善的webui，支持多行命令支持上下键切换历史命令

--- Mods/CommandWebUI/ModConfig.cs
+++ Mods/CommandWebUI/ModConfig.cs
@@ -3,7 +3,7 @@ namespace CommandWebUI
     public class ModConfig
     {
         public int Port = 29103;
-        public int AccessToken = "24641aabcd";
+        public string AccessToken = "24641aabcd";
         public string IndexPage = "index-sv2.html";
         public string LoginPage = "Login.html";
     }
